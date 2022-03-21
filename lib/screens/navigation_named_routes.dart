35af308cefa166f6cd0c357cde895f40878062df
Zaed Abdullah <z4ed.thalib123@gmail.com>
2022-03-21 21:15:46 +0700

    create navigation

--- lib/screens/navigation_named_routes.dart
+++ lib/screens/navigation_named_routes.dart
@@ -12,7 +12,7 @@ class FirstScreen extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            // Navigate to the second screen when tapped.
+            Navigator.pushNamed(context, '/second');
           },
           child: const Text('Launch screen'),
         ),
@@ -33,7 +33,7 @@ class SecondScreen extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            // Navigate back to first screen when tapped.
+            Navigator.pop(context);
           },
           child: const Text('Go back!'),
         ),
