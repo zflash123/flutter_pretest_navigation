297c71807d8e387fbcca9b1e9193089ad6f801b0
Zaed Abdullah <z4ed.thalib123@gmail.com>
2022-03-22 10:35:06 +0700

    edit MateialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_pretest_navigation/screens/navigate_pass_argument.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      routes: {
-        ExtractArgumentsScreen.routeName: (context) =>
-            const ExtractArgumentsScreen(),
-      },
-      onGenerateRoute: (settings) {
-        if (settings.name == PassArgumentsScreen.routeName) {
-          final args = settings.arguments as ScreenArguments;
-
-          return MaterialPageRoute(
-            builder: (context) {
-              return PassArgumentsScreen(
-                title: args.title,
-                message: args.message,
-              );
-            },
-          );
-        }
-        assert(false, 'Need to implement ${settings.name}');
-        return null;
-      },
-      title: 'Navigation with Arguments',
-      home: const HomeScreen(),
-    )
+    const MaterialApp(
+      title: 'Returning Data',
+      home: HomeScreen(),
+    ),
   );
 }
