636e8acbd03f45deb3f3c5a05e40129c7241303d
Zaed Abdullah <z4ed.thalib123@gmail.com>
2022-03-22 09:47:09 +0700

    Add HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,29 @@ import 'package:flutter_pretest_navigation/screens/navigate_pass_argument.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      title: 'Returning Data',
-      home: ExtractArgumentsScreen(),
-    ),
+    MaterialApp(
+      routes: {
+        ExtractArgumentsScreen.routeName: (context) =>
+            const ExtractArgumentsScreen(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == PassArgumentsScreen.routeName) {
+          final args = settings.arguments as ScreenArguments;
+
+          return MaterialPageRoute(
+            builder: (context) {
+              return PassArgumentsScreen(
+                title: args.title,
+                message: args.message,
+              );
+            },
+          );
+        }
+        assert(false, 'Need to implement ${settings.name}');
+        return null;
+      },
+      title: 'Navigation with Arguments',
+      home: const HomeScreen(),
+    )
   );
 }
--- lib/screens/navigate_pass_argument.dart
+++ lib/screens/navigate_pass_argument.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 
-class ScreenArguments {
-  final String title;
-  final String message;
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
-  ScreenArguments(this.title, this.message);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  ExtractArgumentsScreen.routeName,
+                  arguments: ScreenArguments(
+                    'Extract Arguments Screen',
+                    'This message is extracted in the build method.',
+                  ),
+                );
+              },
+              child: const Text('Navigate to screen that extracts arguments'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  PassArgumentsScreen.routeName,
+                  arguments: ScreenArguments(
+                    'Accept Arguments Screen',
+                    'This message is extracted in the onGenerateRoute '
+                        'function.',
+                  ),
+                );
+              },
+              child: const Text('Navigate to a named that accepts arguments'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class ExtractArgumentsScreen extends StatelessWidget {
@@ -22,6 +62,38 @@ class ExtractArgumentsScreen extends StatelessWidget {
       ),
       body: Center(
         child: Text(args.message),
+      ),
+    );
+  }
+}
+
+class ScreenArguments {
+  final String title;
+  final String message;
+
+  ScreenArguments(this.title, this.message);
+}
+
+class PassArgumentsScreen extends StatelessWidget {
+  static const routeName = '/passArguments';
+
+  final String title;
+  final String message;
+
+  const PassArgumentsScreen({
+    Key? key,
+    required this.title,
+    required this.message,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(message),
       ),
     );
   }
